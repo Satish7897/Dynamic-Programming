edaa836a11864e25de8feb717e4d0484da1bd035
Satish7897 <55326969+Satish7897@users.noreply.github.com>
2020-02-05 21:15:53 +0530

    Create climbing Stair leetcode.cpp

---
+++ climbing stair leetcode/climbing Stair leetcode.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a[n+1];
+    a[0]=1;
+    a[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        a[i]=a[i-1]+a[i-2];
+    }
+        return a[n];
+    }
+};
